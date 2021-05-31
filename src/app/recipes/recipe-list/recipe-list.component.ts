711ded0b508025e357a098ce94656ce4106a5c5b
luismangubat <joseluismangubat@gmail.com>
2021-05-31 12:52:14 -0400

    Add List Item component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Test', 'https://images.pexels.com/photos/4346328/pexels-photo-4346328.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')
+  ];
+
 
   constructor() { }
 
